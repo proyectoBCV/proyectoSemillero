bec6f79072a95ed9fa2a642ab539d1b72946531f
natalimercado <n.mercado@uniandes.edu.co>
2023-07-27 03:55:11 +0000

    correciones

--- RESNET.py
+++ RESNET.py
@@ -50,7 +50,7 @@ class CustomDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         image_id = self.data['image'].iloc[index]
-        image_path = f"/media/user_home0/sgoyesp/Proyecto/ISIC_2019_Training_Input/{image_id}.jpg"
+        image_path = f"/home/nmercado/data_proyecto{image_id}.jpg"
         image = Image.open(image_path)
         label = self.data['final_label'].iloc[index]
         if self.transform:
@@ -108,7 +108,6 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 scheduler = StepLR(optimizer, step_size=3, gamma=0.1)
 
 
-#Entrenamiento 
 def train(model, train_loader, criterion, optimizer):
     model.train()
     train_loss = 0.0
@@ -116,9 +115,9 @@ def train(model, train_loader, criterion, optimizer):
     total_samples = 0
     train_predictions = []
     train_labels = []
-    total_steps = 0
+    missclassified_image_ids = []  # Corregido: debe ser missclassified_image_ids en lugar de missclassified_images_ids
 
-    for images, labels, image_id in train_loader:
+    for images, labels, image_ids in train_loader:
         images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
         outputs = model(images)
@@ -131,20 +130,15 @@ def train(model, train_loader, criterion, optimizer):
         total_samples += labels.size(0)
         train_predictions.extend(predicted.cpu().numpy())
         train_labels.extend(labels.cpu().numpy())
-
-        
         missclassified_indices = torch.where(predicted != labels)[0]
-        missclassified_images = images[missclassified_indices].cpu().numpy()
         incorrect_labels = predicted[missclassified_indices].cpu().numpy()
-        correct_labels = labels_batch[missclassified_indices].cpu().numpy()
+        correct_labels = labels[missclassified_indices].cpu().numpy()
+        missclassified_image_ids.extend(image_ids[missclassified_indices])  # Corregido: debe ser image_ids en lugar de image_id
         
-    t_loss = train_loss/len(train_loader)
-    acc = 100 * correct_predictions/total_samples
+    t_loss = train_loss / len(train_loader)
+    acc = 100 * correct_predictions / total_samples
     scheduler.step()
-    return train_predictions, train_labels, t_loss, acc, missclassified_images, incorrect_labels, correct_labels
-    
-
-
+    return train_predictions, train_labels, t_loss, acc, missclassified_image_ids, incorrect_labels, correct_labels
 #Guardar los pesos del entrenamiento
 #torch.save(model.state_dict(), 'modelo_entrenado3.pth')
 
@@ -156,11 +150,11 @@ def evaluate(model, data_loader, criterion):
     total_samples = 0
     predictions = []
     labels = []
-    
+    missclassified_image_ids = []
     
     with torch.no_grad():
         #breakpoint()
-        for images, labels_batch in data_loader:
+        for images, labels_batch, image_ids in data_loader:
             images, labels_batch = images.to(device), labels_batch.to(device)
             outputs = model(images)
             batch_loss = criterion(outputs, labels_batch)
@@ -172,28 +166,28 @@ def evaluate(model, data_loader, criterion):
             labels.extend(labels_batch.cpu().numpy())
             #breakpoint()
             missclassified_indices = torch.where(predicted != labels_batch)[0]
-            missclassified_images = images[missclassified_indices].cpu().numpy()
             incorrect_labels = predicted[missclassified_indices].cpu().numpy()
             correct_labels = labels_batch[missclassified_indices].cpu().numpy()
+            missclassified_image_ids.extend(image_ids[missclassified_indices])
             
             
     avg_loss = loss / len(data_loader)
     accuracy = 100 * correct_predictions / total_samples
 
-    return predictions, labels, avg_loss, accuracy, missclassified_images, incorrect_labels, correct_labels
+    return predictions, labels, avg_loss, accuracy, missclassified_image_ids, incorrect_labels, correct_labels
 
 
-def guardar_data(missclassified_images, incorrect_labels, correct_labels, output_dir):
+def guardar_data(ids_missclassified_images, incorrect_labels, correct_labels, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     for i in range(len(missclassified_images)):
-    image = missclassified_images[i]
-    incorrect_label = incorrect_labels[i]
-    correct_label = correct_labels[i]
-    
-    image_name = f"image_{i}_incorrect_{incorrect_label}_correct_{correct_label}.png"
-    image_path = os.path.join(output_dir, image_name)
-    image_pil.save(image_path)
+        image_path = f"/home/nmercado/data_proyecto{ids_missclassified_images[i]}.jpg"
+        incorrect_label = incorrect_labels[i]
+        correct_label = correct_labels[i]
+        image_pil = Image.open(image_path)
+        image_name = f"image_{i}_incorrect_prediction{incorrect_label}_correct_{correct_label}.png"
+        image_path = os.path.join(output_dir, image_name)
+        image_pil.save(image_path)
     
 train_loss=[]
 val_loss=[]
@@ -204,91 +198,84 @@ best_model_state_dict = None
 
 
 output_train_directory = "./train_missclassified_images"
+output_val_directory = "./val_missclassified_images"
+output_test_directory = "./test_missclassified_images"
 
-for epoch in range(num_epochs):
-    #impresión train
-    train_predictions, train_labels, t_loss, acc = train(model, train_loader, criterion, optimizer)
-    train_precision = precision_score(train_labels, train_predictions, average=None,zero_division=1)
-    train_recall = recall_score(train_labels, train_predictions, average=None)
-    train_f1_score = f1_score(train_labels, train_predictions, average=None)
-    train_loss.append(t_loss)
+output_results_file = "results.txt"
 
-    print(f'Época: {epoch+1:.4f}')
-    print(f'Training Loss: {t_loss:.4f} | Training Accuracy: {acc:.2f}%')
-    print(f'Training Precision: {train_precision}')
-    print(f'Training Recall: {train_recall}')
-    print(f'Training F1-Score: {train_f1_score}')
-    print('---------------------------')
+with open(output_results_file, 'w') as f:
+    for epoch in range(num_epochs):
+        #impresión train
+        train_predictions, train_labels, t_loss, acc, train_missclassified_images, train_incorrect_labels, train_correct_labels = train(model, train_loader, criterion, optimizer)
+        train_precision = precision_score(train_labels, train_predictions, average=None,zero_division=1)
+        train_recall = recall_score(train_labels, train_predictions, average=None)
+        train_f1_score = f1_score(train_labels, train_predictions, average=None)
+        train_loss.append(t_loss)
+
+        guardar_data(train_missclassified_images, train_incorrect_labels, train_correct_labels, output_train_directory)
+
+        print(f'Época: {epoch+1:.4f}', file=f)
+        print(f'Training Loss: {t_loss:.4f} | Training Accuracy: {acc:.2f}%', file=f)
+        print(f'Training Precision: {train_precision}', file=f)
+        print(f'Training Recall: {train_recall}', file=f)
+        print(f'Training F1-Score: {train_f1_score}', file=f)
+        print('---------------------------', file=f)
+        
+        #Validación
+        valid_predictions, valid_labels, v_loss, v_acc, val_missclassified_images, val_incorrect_labels, val_correct_labels = evaluate(model, valid_loader, criterion) #,valid_images, valid_label_true, valid_label_pred = evaluate(model, valid_loader, criterion)  
+        valid_precision = precision_score(valid_labels, valid_predictions, average=None, zero_division=1)
+        valid_recall = recall_score(valid_labels, valid_predictions, average=None)
+        valid_f1_score = f1_score(valid_labels, valid_predictions, average=None)
+        valid_accuracy= accuracy_score(valid_labels, valid_predictions, normalize=True)
+        val_loss.append(v_loss)
+
+        guardar_data(val_missclassified_images, val_incorrect_labels, output_val_directory)
+    
+        if v_loss < best_val_loss:
+            best_val_loss = v_loss
+            # Guardar los pesos del modelo en este punto (última capa)
+            best_model_state_dict = model.state_dict()
+
+        print('---------- Validación ----------', file=f)
+        print(f'Validation Loss: {v_loss:.4f} | Validation Accuracy: {v_acc:.2f}%', file=f)
+        print(f'Validation Precision: {valid_precision}', file=f)
+        print(f'Validation Recall: {valid_recall}', file=f)
+        print(f'Validation F1-Score: {valid_f1_score}', file=f)
+        print(f'Validation accuracy: {valid_accuracy}', file=f)
+        print('-------------------------------', file=f)
 
     
-    #Validación
-    valid_predictions, valid_labels, v_loss, v_acc, val_missclassified_images, val_incorrect_labels, val_correct_labels = evaluate(model, valid_loader, criterion) #,valid_images, valid_label_true, valid_label_pred = evaluate(model, valid_loader, criterion)  
-    valid_precision = precision_score(valid_labels, valid_predictions, average=None, zero_division=1)
-    valid_recall = recall_score(valid_labels, valid_predictions, average=None)
-    valid_f1_score = f1_score(valid_labels, valid_predictions, average=None)
-    valid_accuracy= accuracy_score(valid_labels, valid_predictions, normalize=False)
-    val_loss.append(v_loss)
+        # Evaluación en el conjunto de prueba
+        test_predictions, test_labels, t_loss, t_acc, test_missclassified_images, test_incorrect_labels, test_correct_labels = evaluate(model, test_loader, criterion) #, test_images, test_label_true, test_label_pred = evaluate(model, test_loader, criterion)
+        test_precision = precision_score(test_labels, test_predictions, average=None, zero_division=1)
+        test_recall = recall_score(test_labels, test_predictions, average=None)
+        test_f1_score = f1_score(test_labels, test_predictions, average=None)
+        test_accuracy= accuracy_score(test_labels, test_predictions, normalize=True)
+        test_loss.append(t_loss)
 
-    if v_loss < best_val_loss:
-        best_val_loss = v_loss
-        # Guardar los pesos del modelo en este punto (última capa)
-        best_model_state_dict = model.state_dict()
-    #guardar_data(val_missclassified_images, val_incorrect_labels, val_correct_labels, output_directory)
-    print('---------- Validación ----------')
-    print(f'Validation Loss: {v_loss:.4f} | Validation Accuracy: {v_acc:.2f}%')
-    print(f'Validation Precision: {valid_precision}')
-    print(f'Validation Recall: {valid_recall}')
-    print(f'Validation F1-Score: {valid_f1_score}')
-    print(f'Validation accuracy: {valid_accuracy}')
-    print('-------------------------------')
+        guardar_data(test_missclassified_images, test_incorrect_labels, test_correct_labels, output_test_directory)
     
-    
-    # Evaluación en el conjunto de prueba
-    test_predictions, test_labels, t_loss, t_acc, train_missclassified_images, train_incorrect_labels, train_correct_labels = evaluate(model, test_loader, criterion) #, test_images, test_label_true, test_label_pred = evaluate(model, test_loader, criterion)
-    test_precision = precision_score(test_labels, test_predictions, average=None, zero_division=1)
-    test_recall = recall_score(test_labels, test_predictions, average=None)
-    test_f1_score = f1_score(test_labels, test_predictions, average=None)
-    test_accuracy= accuracy_score(test_labels, test_predictions, normalize=False)
-    test_loss.append(t_loss)
+        print('---------- Prueba ----------', file=f)
+        print(f'Test Loss: {t_loss:.4f} | Test Accuracy: {t_acc:.2f}%', file=f)
+        print(f'Test Precision: {test_precision}', file=f)
+        print(f'Test Recall: {test_recall}', file=f)
+        print(f'Test F1-Score: {test_f1_score}', file=f)
+        print(f'Test accuracy: {test_accuracy}', file=f)
+        print(f"Learning Rate: {scheduler.get_last_lr()[0]}", file=f)
+        print('----------------------------', file=f)
 
-    #guardar_data(train_missclassified_images, train_incorrect_labels, train_correct_labels, output_directory)
-    
-    print('---------- Prueba ----------')
-    print(f'Test Loss: {t_loss:.4f} | Test Accuracy: {t_acc:.2f}%')
-    print(f'Test Precision: {test_precision}')
-    print(f'Test Recall: {test_recall}')
-    print(f'Test F1-Score: {test_f1_score}')
-    print(f'Test accuracy: {test_accuracy}')
-    print(f"Learning Rate: {scheduler.get_last_lr()[0]}")
-    print('----------------------------')
     
 torch.save(best_model_state_dict, 'mejor_modelo.pth')
 
 plt.figure()
-plt.plot(range(1, num_epochs + 1), train_loss, label='Training Loss')
+plt.plot(range(1, num_epochs + 1), train_loss, label='Training Loss', color='blue')
+plt.plot(range(1, num_epochs + 1), val_loss, label='Validation Loss', color='green')
+plt.plot(range(1, num_epochs + 1), test_loss, label='Test Loss', color='red')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
-plt.title('Training Loss')
+plt.title('Loss')
 plt.legend()
-plt.savefig("train_loss.png")
-plt.close()
-
-plt.figure()
-plt.plot(range(1, num_epochs + 1), val_loss, label='Validation Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.title('Validation Loss')
-plt.legend()
-plt.savefig("val_loss.png")
-plt.close()
-
-plt.figure()
-plt.plot(range(1, num_epochs + 1), test_loss, label='Test Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.title('Test Loss')
-plt.legend()
-plt.savefig("test_loss.png")
+plt.savefig("losses.png")
 plt.close()
 
 end_time = time.time()
